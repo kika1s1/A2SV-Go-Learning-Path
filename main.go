8786d4568fe8096c6e39d53798c874a4f9f47dd4
kika1s1 <tamiratkebede120@gmail.com>
2024-07-29 14:39:12 +0300

    switcha and boolean

--- main.go
+++ main.go
@@ -5,53 +5,39 @@ import (
 )
 
 func main() {
-    // looping in go
-    // for loop
-    // syntax
-    // for initialization; condition; post {
-    //     // block of code
-    // }
-    // initialization
-    i := 0
+    // boolean 
+    // true or false
+    var b bool = true
+    fmt.Println(b)
     // condition
-    for i < 10 {
-        // block of code
-        fmt.Println(i)
-        // post
-        i++
+    if b {
+        fmt.Println("true")
+    } else {
+        fmt.Println("false")
     }
-    // loop without initialization
-    for j := 0; j < 10; j++ {
-        fmt.Println(j)
+    // switch statement
+    // switch expression
+    // expression is evaluated once
+    // then match with case
+    // if no case match then default case
+    switch 1 + 2 {
+    case 1:
+        fmt.Println("one")
+    case 2:
+        fmt.Println("two")
+    default:
+        fmt.Println("default")
     }
-    // loop without condition
-    for {
-        fmt.Println("infinite loop")
-        break
-    }
-    // loop with break statement
-    for k := 0; k < 10; k++ {
-        if k == 5 {
-            break
-        }
-        fmt.Println(k)
-    }
-    // loop with continue statement
-    for l := 0; l < 10; l++ {
-        if l % 2 == 0 {
-            continue
-        }
-        fmt.Println(l)
-    }
-    // loop with label
-    outerLoop:
-    for m := 0; m < 10; m++ {
-        for n := 0; n < 10; n++ {
-            if n == 5 {
-                break outerLoop
-            }
-            fmt.Println(m, n)
-        }
+    // fallthrough keyword
+    // it allows to fall through to next case
+    switch 1 + 2 {
+    case 1:
+        fmt.Println("one")
+        fallthrough
+    case 2:
+        fmt.Println("two")
+    default:
+        fmt.Println("default")
     }
 
 }
