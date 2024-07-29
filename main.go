dff1c9ddfad9720d07092dd72fd4610f110056ef
kika1s1 <tamiratkebede120@gmail.com>
2024-07-29 11:12:12 +0300

    Refactor main.go to use fmt.Print instead of fmt.Println

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ import (
     "fmt"
 )
 
+// to run go file 
+    // - go run main.go
+// main function in entry function
+// only main function one in one package
+// what is fmt ?
+// it is just formatting package
+// everything in fmt dot should start with Capital letter
+// example
+        // - fmt.Println("hello world")
+        // - fmt.Print("hello world")
 func main(){
     fmt.Print("Initial Go lang")
     // this is just for format string or output code
