9c998e4ee844f2aae60bd93b1e613a6f8b5b06be
kika1s1 <tamiratkebede120@gmail.com>
2024-07-29 14:27:05 +0300

    loops in go

--- main.go
+++ main.go
@@ -2,24 +2,56 @@ package main
 
 import (
     "fmt"
-    "strings"
-    "sort" 
 )
 
 func main() {
-    nums := []int{4,2,1,3,4,5,54,643}
-    greetings := "hello this is tamirat kebede"
-    fmt.Println(strings.Contains(greetings, "hello"))
-    fmt.Println(strings.ReplaceAll(greetings, "e", "*"))
-    fmt.Println(strings.ToUpper(greetings))
-    fmt.Println(greetings)
-    fmt.Println(strings.Index(greetings, "hello"))
-    fmt.Println(strings.Split(greetings, "l"))
-    fmt.Println(nums)
-    for i:=0; i < len(nums); i++{
-        fmt.Println(nums[i])
+    // looping in go
+    // for loop
+    // syntax
+    // for initialization; condition; post {
+    //     // block of code
+    // }
+    // initialization
+    i := 0
+    // condition
+    for i < 10 {
+        // block of code
+        fmt.Println(i)
+        // post
+        i++
     }
-    // nums.sort()
-    sort.Ints(nums)
-    fmt.Println(nums)
+    // loop without initialization
+    for j := 0; j < 10; j++ {
+        fmt.Println(j)
+    }
+    // loop without condition
+    for {
+        fmt.Println("infinite loop")
+        break
+    }
+    // loop with break statement
+    for k := 0; k < 10; k++ {
+        if k == 5 {
+            break
+        }
+        fmt.Println(k)
+    }
+    // loop with continue statement
+    for l := 0; l < 10; l++ {
+        if l % 2 == 0 {
+            continue
+        }
+        fmt.Println(l)
+    }
+    // loop with label
+    outerLoop:
+    for m := 0; m < 10; m++ {
+        for n := 0; n < 10; n++ {
+            if n == 5 {
+                break outerLoop
+            }
+            fmt.Println(m, n)
+        }
+    }
+
 }
