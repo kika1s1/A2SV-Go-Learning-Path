26a8d60e9d041a3ad3e6e92ac82debfce21c71b1
kika1s1 <tamiratkebede120@gmail.com>
2024-07-29 14:40:37 +0300

    if else and else if

--- main.go
+++ main.go
@@ -5,39 +5,14 @@ import (
 )
 
 func main() {
-    // boolean 
-    // true or false
-    var b bool = true
-    fmt.Println(b)
-    // condition
-    if b {
-        fmt.Println("true")
-    } else {
-        fmt.Println("false")
-    }
-    // switch statement
-    // switch expression
-    // expression is evaluated once
-    // then match with case
-    // if no case match then default case
-    switch 1 + 2 {
-    case 1:
-        fmt.Println("one")
-    case 2:
-        fmt.Println("two")
-    default:
-        fmt.Println("default")
-    }
-    // fallthrough keyword
-    // it allows to fall through to next case
-    switch 1 + 2 {
-    case 1:
-        fmt.Println("one")
-        fallthrough
-    case 2:
-        fmt.Println("two")
-    default:
-        fmt.Println("default")
+    // if else
+    age :=12
+    if age > 18 {
+        fmt.Println("You are an adult")
+    } else if age ==12 {
+        fmt.Println("You are a teenager")
+    }else {
+        fmt.Println("You are a child")
     }
 
 }
