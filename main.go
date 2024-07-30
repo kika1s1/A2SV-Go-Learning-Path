b11fa4d02644f22c5ec6813e29f3182cfb3bd5da
kika1s1 <tamiratkebede120@gmail.com>
2024-07-30 10:09:55 +0300

    structure data structure is finished

--- main.go
+++ main.go
@@ -5,31 +5,5 @@ import (
 )
 
 func main() {
-    information := []struct {
-        name string
-        age  int
-    }{
-        {"John", 25},
-        {"Jane", 30},
-        {"Mike", 35},
-    }
-    // Add more information to the information variable if needed
     
-    information = append(information, struct {
-        name string
-        age  int
-    }{
-        name: "Sarah",
-        age:  40,
-    }, struct {
-        name string
-        age  int
-    }{
-        name: "David",
-        age:  45,
-    })
-    fmt.Println("Name\tAge")
-    for _, person := range information {
-        fmt.Printf("%s\t%d\n", person.name, person.age)
-    }
 }
