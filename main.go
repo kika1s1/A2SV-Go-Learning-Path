455a899a966cc239b55b76e47b75013eed24af7e
kika1s1 <tamiratkebede120@gmail.com>
2024-07-30 10:26:29 +0300

    nil is  just None

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import (
-    "fmt"
-)
+import "fmt"
 
-func main() {}
+func main() {
+    // s := []int{}
+    var s []int
+    // fmt.Printf("%d \n", len(s))
+    if s == nil {
+        fmt.Println("Slice is nil")
+    }
+}
+
