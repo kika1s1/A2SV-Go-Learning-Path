15592804703ca1cee909269082d7673233163964
kika1s1 <tamiratkebede120@gmail.com>
2024-07-30 11:05:51 +0300

    different way of declearing map

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import "fmt"
-
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m map[string]Vertex
 func main() {
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
-	}
+	m := make(map[string]int)
+	// m := map[string]int{}
+
+
+	m["Answer"] = 42
+    m["this"] = 0
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+    if m["Answer"] == m["this"]{
+        fmt.Print("YES")
+    }
     
-	fmt.Println(m["Bell Labs"])
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+    fmt.Println(m)
 }
