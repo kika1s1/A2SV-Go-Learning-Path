d7fb4c9cfb9cd4ee287af2c0f86db7b6ccb7e5c1
kika1s1 <tamiratkebede120@gmail.com>
2024-07-30 11:10:12 +0300

    fibonacci

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
-
 import "fmt"
-func main() {
-	m := make(map[string]int)
-	// m := map[string]int{}
-
-
-	m["Answer"] = 42
-    m["this"] = 0
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-    if m["Answer"] == m["this"]{
-        fmt.Print("YES")
+func fibonacci(n int)int{
+    if n <= 1 {
+        return n
     }
-    
-	fmt.Println("The value:", m["Answer"])
+    return fibonacci(n-1) + fibonacci(n-2)
+}
 
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
-    fmt.Println(m)
+func main() {
+    f := fibonacci
+	for i := 0; i < 10; i++ {
+		fmt.Println(f(i))
+	}
 }
