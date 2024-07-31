f3adfc767e6915d3555263bc37b1234671086989
kika1s1 <tamiratkebede120@gmail.com>
2024-07-31 15:25:43 +0300

    finished version of task contorller

--- task_manager_api/controllers/tasks_controller.go
+++ task_manager_api/controllers/tasks_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
     "github.com/gin-gonic/gin"
 	"github.com/kika1s1/task_manager_api/data"
+	"github.com/kika1s1/task_manager_api/models"
 
 )
 
@@ -20,6 +21,62 @@ func GetTask(c *gin.Context) {
             return
         }
     }
-
     c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+}
+
+
+ func UpdateTask(ctx *gin.Context) {
+    id := ctx.Param("id")
+
+    var updatedTask models.Task
+
+    if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    for i, task := range data.Tasks {
+        if task.ID == id {
+            // Update only the specified fields
+            if updatedTask.Title != "" {
+                data.Tasks[i].Title = updatedTask.Title
+            }
+            if updatedTask.Description != "" {
+                data.Tasks[i].Description = updatedTask.Description
+            }
+            ctx.JSON(http.StatusOK, gin.H{"message": "Task updated"})
+            return
+        }
+    }
+
+    ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+
+
+func DeleteTask(ctx *gin.Context) {
+    id := ctx.Param("id")
+
+    for i, val := range data.Tasks {
+        if val.ID == id {
+            data.Tasks = append(data.Tasks[:i], data.Tasks[i+1:]...)
+            ctx.JSON(http.StatusOK, gin.H{"message": "Task removed"})
+            return
+        }
+    }
+
+    ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+
+func CreateTask(ctx *gin.Context) {
+    var newTask models.Task
+
+    if err := ctx.ShouldBindJSON(&newTask); err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    data.Tasks = append(data.Tasks, newTask)
+    ctx.JSON(http.StatusCreated, gin.H{"message": "Task created"})
 }
