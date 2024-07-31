bbc13d07c1b4db582896c4cf660471cfa6298253
kika1s1 <tamiratkebede120@gmail.com>
2024-07-31 14:41:21 +0300

    task dummy data is added

---
+++ task_manager_api/data/tasks.go
@@ -0,0 +1,12 @@
+package data
+
+import (
+	"github.com/kika1s1/task_manager_api/models"
+	"time"
+)
+
+var tasks = []models.Task{
+	{ID: "1", Title: "Task 1", Description: "First task", DueDate: time.Now(), Status: "Pending"},
+	{ID: "2", Title: "Task 2", Description: "Second task", DueDate: time.Now().AddDate(0, 0, 1), Status: "In Progress"},
+	{ID: "3", Title: "Task 3", Description: "Third task", DueDate: time.Now().AddDate(0, 0, 2), Status: "Completed"},
+}
