fd2d6ec20b278a9bf5ae2f128383008e488ea9bf
kika1s1 <tamiratkebede120@gmail.com>
2024-08-03 14:45:46 +0300

    user model is added

---
+++ task_manager/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+
+
+type User struct {
+	ID       string `bson:"_id,omitempty" json:"id,omitempty"`
+	Username string `bson:"username" json:"username"`
+	Password string `bson:"password" json:"password"`
+	Role     string `bson:"role" json:"role"`
+}
+
