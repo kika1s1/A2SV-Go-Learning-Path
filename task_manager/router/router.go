e88e1f17f13dd69c907c4bf929a3504a29e9701e
kika1s1 <tamiratkebede120@gmail.com>
2024-08-03 14:43:32 +0300

    router protect route is added

--- task_manager/router/router.go
+++ task_manager/router/router.go
@@ -1,18 +1,28 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	"github.com/kika1s1/task_manager/controllers"
-
-    
+	"github.com/kika1s1/task_manager/middleware"
 )
 
-func SetRoutes(router *gin.Engine) {
-	router.GET("/tasks", controllers.GetAllTasks)
-	router.GET("/tasks/:id", controllers.GetTask)
-	router.PUT("/tasks/:id", controllers.UpdateTask)
-	router.DELETE("/tasks/:id", controllers.DeleteTask)
-	router.POST("/tasks", controllers.CreateTask)
+func SetupRouter()*gin.Engine {
+	router :=gin.Default()
+	router.POST("/auth/register", controllers.Register)
+	router.POST("/auth/login", controllers.Login)
+
+	// Protected routes
+	protected := router.Group("/tasks")
+	protected.Use(middleware.AuthMiddleware(), middleware.AdminMiddleware())
+	{
+		protected.GET("/tasks", controllers.GetAllTasks)
+		protected.GET("/task/:id", controllers.GetTask)
+		protected.PUT("/task/:id", controllers.UpdateTask)
+		protected.DELETE("/task/:id", controllers.DeleteTask)
+		protected.POST("/task", controllers.CreateTask)
+	}
+	return router
+	
 	
 
 }
