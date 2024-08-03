a7a6b5b8e18cdf636c210173bd91e3d7c0e7b355
kika1s1 <tamiratkebede120@gmail.com>
2024-08-03 14:44:40 +0300

    auth middleware is added

---
+++ task_manager/middleware/auth_middleware.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/kika1s1/task_manager/models"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    return func(c *gin.Context) {
+        tokenString := c.GetHeader("Authorization")
+        if tokenString == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+            c.Abort()
+            return
+        }
+
+        tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+        claims := &models.Claims{}
+        token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+            return []byte(os.Getenv("JWT_SECRET")), nil
+        })
+
+        if err != nil || !token.Valid {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+            c.Abort()
+            return
+        }
+
+        c.Set("user", claims)
+        c.Next()
+    }
+}
