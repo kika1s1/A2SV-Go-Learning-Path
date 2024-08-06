c868a7d84d5cf37b23c4a6a279f71d4fd4e90eb9
kika1s1 <tamiratkebede120@gmail.com>
2024-08-06 21:47:35 +0300

    middleware is added

---
+++ task_manager/internal/infrastructure/middleware/auth_middleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+    "net/http"
+    "strings"
+
+    "github.com/dgrijalva/jwt-go"
+    "github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(jwtSecret string) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        authHeader := c.GetHeader("Authorization")
+        if authHeader == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+            c.Abort()
+            return
+        }
+
+        tokenString := strings.Split(authHeader, "Bearer ")[1]
+        token, err := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+            return []byte(jwtSecret), nil
+        })
+
+        if err != nil || !token.Valid {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+            c.Abort()
+            return
+        }
+
+        claims, ok := token.Claims.(*jwt.StandardClaims)
+        if !ok {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+            c.Abort()
+            return
+        }
+
+        c.Set("userID", claims.Subject)
+        c.Next()
+    }
+}
